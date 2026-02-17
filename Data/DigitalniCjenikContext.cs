176fd883537250114c2021b8b021ffa7addd40cd
drusak21 <drusak21@student.foi.hr>
2026-02-17 12:01:56 +0100

    Add project files.

---
+++ Data/DigitalniCjenikContext.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace DigitalniCjenik.Data
+{
+    public class DigitalniCjenikContext : DbContext
+    {
+        public DigitalniCjenikContext(DbContextOptions<DigitalniCjenikContext> options)
+            : base(options)
+        {
+        }
+
+       // public DbSet<Uloga> Uloga { get; set; }
+        //public DbSet<Korisnik> Korisnici { get; set; }
+
+    }
+}
