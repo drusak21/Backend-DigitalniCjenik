31617a12be95ced4b2efd14a9442d2def8e2315e
drusak21 <drusak21@student.foi.hr>
2026-02-18 09:47:26 +0100

    Add JWT in Program.cs

--- Program.cs
+++ Program.cs
@@ -1,22 +1,65 @@
 using DigitalniCjenik.Data;
+using DigitalniCjenik.Security;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// DbContext
 builder.Services.AddDbContext<DigitalniCjenikContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("localDatabase"))
+    options.UseNpgsql(
+        builder.Configuration.GetConnectionString("localDatabase")
+    )
 );
+
+// JWT settings
+builder.Services.Configure<JwtSettings>(
+    builder.Configuration.GetSection("JwtSettings")
+);
+
+// Authentication (JWT)
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+.AddJwtBearer(options =>
+{
+    var jwt = builder.Configuration
+        .GetSection("JwtSettings")
+        .Get<JwtSettings>();
+
+    if (string.IsNullOrEmpty(jwt?.Key))
+    {
+        throw new InvalidOperationException("JWT Key must be provided in configuration.");
+    }
+
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+
+        ValidIssuer = jwt.Issuer,
+        ValidAudience = jwt.Audience,
+
+        IssuerSigningKey = new SymmetricSecurityKey(
+            Encoding.UTF8.GetBytes(jwt.Key)
+        )
+    };
+});
+
+// Authorization
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -25,6 +68,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
