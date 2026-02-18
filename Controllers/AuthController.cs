9279bd7bf86e03f1b57084ac4013cff7fc082768
drusak21 <drusak21@student.foi.hr>
2026-02-18 09:34:37 +0100

    Added AuthController

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,28 @@
+﻿using DigitalniCjenik.DTO;
+using DigitalniCjenik.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DigitalniCjenik.Controllers
+{
+    [ApiController]
+    [Route("api/auth")]
+    public class AuthController : ControllerBase
+    {
+        private readonly AuthService _authService;
+
+        public AuthController(AuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login(LoginRequestDTO request)
+        {
+            var result = _authService.Login(request);
+            if (result == null)
+                return Unauthorized();
+
+            return Ok(result);
+        }
+    }
+}
